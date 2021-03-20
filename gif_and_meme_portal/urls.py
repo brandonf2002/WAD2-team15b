9e9e4be4505af0248edb9e977be80f6f6e1b8147
Harry Goodwin <harrybmg@gmail.com>
2021-03-20 11:00:54 +0000

    added some static files

--- gif_and_meme_portal/urls.py
+++ gif_and_meme_portal/urls.py
@@ -17,8 +17,10 @@ from meme_portal import views
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('meme_portal.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
