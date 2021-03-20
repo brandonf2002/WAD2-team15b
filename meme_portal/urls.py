b5ee47e998462fc4fe660685ae949bd0b29b27c9
Harry Goodwin <harrybmg@gmail.com>
2021-03-20 11:29:04 +0000

    added nav bar to base

--- meme_portal/urls.py
+++ meme_portal/urls.py
@@ -4,5 +4,6 @@ from meme_portal import views
 app_name = 'meme_portal'
 
 urlpatterns = [
-    path('', views.index, name='meme_portal_index')
+    path('', views.index, name='meme_portal_index'),
+	
 ]
