66d6420c8a8585a5c3787f8fe1fa7fac7d6a5a59
Harry Goodwin <harrybmg@gmail.com>
2021-03-20 12:18:34 +0000

    added some basic views

--- meme_portal/urls.py
+++ meme_portal/urls.py
@@ -5,5 +5,8 @@ app_name = 'meme_portal'
 
 urlpatterns = [
     path('', views.index, name='meme_portal_index'),
-	
+	path('register/', views.register, name='register'),
+	path('login/', views.user_login, name='login'),
+	path('logout/', views.user_logout,name='logout'),
+	path('create/', views.user_logout,name='create'),
 ]
--- meme_portal/views.py
+++ meme_portal/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'meme_portal/index.html')
+	
+def create_page(request, category_name_slug):
+	return render(request, 'meme_portal/create.html')
+	
+def register(request):
+	return render(request, 'meme_portal/register.html')
+	
+def user_login(request):
+	return render(request, 'meme_portal/login.html')
+	
+def user_logout(request):
+	return render(request, 'meme_portal/logout.html')
