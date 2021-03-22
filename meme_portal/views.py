5334994ff5db46b4ad313aa129b748c97bed506e
Harry Goodwin <harrybmg@gmail.com>
2021-03-22 11:28:15 +0000

    some view changes

--- meme_portal/views.py
+++ meme_portal/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     return render(request, 'meme_portal/index.html')
-    
+  
+@login_required  
 def create_page(request, category_name_slug):
     return render(request, 'meme_portal/create.html')
     
@@ -12,9 +14,6 @@ def register(request):
 def user_login(request):
     return render(request, 'meme_portal/login.html')
     
-def user_logout(request):
-    return render(request, 'meme_portal/logout.html')
-    
 def user_account(request):
     return render(request, 'meme_portal/account.html')
     
@@ -23,3 +22,10 @@ def create(request):
 
 def forum(request):
     return render(request, 'meme_portal/forum.html')
+
+	
+@login_required
+def user_logout(request):
+	logout(request)
+	return redirect(reverse('meme_portal/index.html'))
+
