1fa222e3b51cfe1e4d9603ca330a2f0417664852
Harry Goodwin <harrybmg@gmail.com>
2021-03-22 11:45:47 +0000

    login and some authentication

--- meme_portal/admin.py
+++ meme_portal/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from meme_portal.models import Forum, Post, Comment
+from meme_portal.models import Forum, Post, Comment, UserProfile
 
 admin.site.register(Forum)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(UserProfile)
+
