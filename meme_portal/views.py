b7533820b4418bff8c3e37df2013349d759d888d
Harry Goodwin <harrybmg@gmail.com>
2021-03-23 10:31:35 +0000

    some work on posts output..

--- meme_portal/views.py
+++ meme_portal/views.py
@@ -7,9 +7,19 @@ from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from meme_portal.models import Post
 
 def index(request):
-    return render(request, 'meme_portal/index.html')
+	# Query the database for a list of ALL categories currently stored.
+	# Order the categories by the number of likes in descending order.
+	# Retrieve the top 5 only -- or all if less than 5.
+	# Place the list in our context_dict dictionary (with our boldmessage!)
+	# that will be passed to the template engine.
+	posts_list = Post.objects.order_by('-likes')[:5]
+	context_dict = {}
+	context_dict['posts'] = posts_list
+	# Render the response and send it back!
+	return render(request, 'meme_portal/index.html', context=context_dict)
   
 @login_required  
 def create_page(request, category_name_slug):
