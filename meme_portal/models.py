0e6a801135f87a9e5441789de412506f2cf09298
Brandon Forrest <brandonf2002@live.co.uk>
2021-03-29 09:30:25 +0100

    Gave all posts and comments and author

--- meme_portal/models.py
+++ meme_portal/models.py
@@ -3,6 +3,17 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
+class UserProfile(models.Model):
+	# This line is required. Links UserProfile to a User model instance.
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	# The additional attributes we wish to include.
+	email = models.EmailField(blank=True)
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+
+	def __str__(self):
+		return self.user.username
+
 class Forum(models.Model):
     name = models.CharField(max_length=128, unique=True)
     slug = models.SlugField(unique=True);
@@ -20,8 +31,7 @@ class Post(models.Model):
     img_url = models.URLField()
     time_posted = models.DateTimeField(default=timezone.now)
     likes = models.IntegerField(default=0)
-    ##########  RE_INSTATE ONCE USERS CAN BE CREATED  ################
-    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name;
@@ -30,20 +40,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
     time_posted = models.DateTimeField(default=timezone.now)
-    ##########  RE_INSTATE ONCE USERS CAN BE CREATED  ################
-    #author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.content
-
-class UserProfile(models.Model):
-	# This line is required. Links UserProfile to a User model instance.
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	# The additional attributes we wish to include.
-	email = models.EmailField(blank=True)
-	website = models.URLField(blank=True)
-	picture = models.ImageField(upload_to='profile_images', blank=True)
-
-	def __str__(self):
-		return self.user.username
-
