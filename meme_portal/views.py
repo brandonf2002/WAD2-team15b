57487bcf458c3a0dd802e445379c84557e4f63d1
Brandon Forrest <brandonf2002@live.co.uk>
2021-03-30 17:37:04 +0100

    added some basic JQuery/ajax to forum.html

--- meme_portal/views.py
+++ meme_portal/views.py
@@ -180,17 +180,17 @@ def forum(request):
     return show_forum(request, forum_slug)
 
 def like_link(request, forum_name_slug, post_name_slug):
-    print(post_name_slug)
-    post = get_object_or_404(Post, slug=post_name_slug)
-    print(post.likes.count())
-    #if request.method == 'POST':
-    usr = request.user
-    usrProf = get_object_or_404(UserProfile, user=usr)
-    if usr.is_authenticated:
-        if usrProf in post.likes.all():
-            post.likes.remove(usrProf)
-        else:
-            post.likes.add(usrProf)
+    if request.method == 'GET':
+        print(post_name_slug)
+        post = get_object_or_404(Post, slug=post_name_slug)
+        print(post.likes.count())
+        usr = request.user
+        usrProf = get_object_or_404(UserProfile, user=usr)
+        if usr.is_authenticated:
+            if usrProf in post.likes.all():
+                post.likes.remove(usrProf)
+            else:
+                post.likes.add(usrProf)
     return show_forum(request, forum_name_slug)
 
 @login_required
