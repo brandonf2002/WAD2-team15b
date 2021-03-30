943abfe22b171e5273f507951751e73d5f90dbdd
Brandon Forrest <brandonf2002@live.co.uk>
2021-03-30 16:41:01 +0100

    added some backend like functionality

--- meme_portal/views.py
+++ meme_portal/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic import RedirectView
-from meme_portal.models import Post, Forum, Comment
+from meme_portal.models import Post, Forum, Comment, UserProfile
 from datetime import datetime
 
 def index(request):
@@ -179,19 +179,19 @@ def forum(request):
     forum_slug = Forum.objects.order_by('?')[0].slug
     return show_forum(request, forum_slug)
 
-class PostLikeRedirect(RedirectView):
-    def get_redirect_url(self, *args, **kwargs):
-        slug = self.kwargs.get("slug")
-        print(slug)
-        post = get_object_or_404(Post, slug=slug)
-        url_ = post.get_absolute_url()
-        usr = self.request.user
-        if usr.is_authenticated():
-            if usr in post.likes.all():
-                post.likes.remove(usr)
-            else:
-                post.likes.add(usr)
-        return url_
+def like_link(request, forum_name_slug, post_name_slug):
+    print(post_name_slug)
+    post = get_object_or_404(Post, slug=post_name_slug)
+    print(post.likes.count())
+    #if request.method == 'POST':
+    usr = request.user
+    usrProf = get_object_or_404(UserProfile, user=usr)
+    if usr.is_authenticated:
+        if usrProf in post.likes.all():
+            post.likes.remove(usrProf)
+        else:
+            post.likes.add(usrProf)
+    return show_forum(request, forum_name_slug)
 
 @login_required
 def user_logout(request):
