101514084c45e198dc4877ada304ab8794785483
Brandon Forrest <brandonf2002@live.co.uk>
2021-03-31 00:07:53 +0100

    Updated custom template tags

--- meme_portal/templatetags/forum_tags.py
+++ meme_portal/templatetags/forum_tags.py
@@ -1,7 +1,16 @@
 from django import template
+from django.shortcuts import get_object_or_404
+from meme_portal.models import UserProfile
 
 register = template.Library()
 
 @register.simple_tag
 def subtract(a, b):
     return a - b
+
+@register.simple_tag
+def isIn(usr, mamyToManyRel):
+    if not usr.is_authenticated:
+        return False
+    usrProf = get_object_or_404(UserProfile, user=usr)
+    return usrProf in mamyToManyRel.all()
