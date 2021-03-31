66b8fc5c341ae6db669158a4fff8d330af8669ad
Brandon Forrest <brandonf2002@live.co.uk>
2021-03-31 22:36:12 +0100

    added a forum and some like/dislike functionality to post view

--- meme_portal/forms.py
+++ meme_portal/forms.py
@@ -1,21 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from meme_portal.models import UserProfile, Post
+from meme_portal.models import UserProfile, Post, Comment
 
 class UserForm(forms.ModelForm):
-	password = forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput())
 
-	class Meta:
-		model = User
-		fields = ('username', 'email', 'password',)
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
 
 class UserProfileForm(forms.ModelForm):
-	class Meta:
-		model = UserProfile
-		fields = ('website', 'picture',)
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
 
 class PostForm(forms.ModelForm):
-	class Meta:
-		model=Post
-		fields=('name','img_url')
+    class Meta:
+        model=Post
+        fields=('name','img_url')
