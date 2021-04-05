c6faa500890c5e4d5657afab11a8ad331d499cb6
Harry Goodwin <2557827g@student.gla.ac.uk>
2021-04-05 11:43:07 +0100

    login

--- meme_portal/views.py
+++ meme_portal/views.py
@@ -17,6 +17,8 @@ from django.db.models.query_utils import Q
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
 def index(request):
     forums_with_posts = Forum.objects.filter(posts__isnull=False).distinct()
@@ -130,26 +132,14 @@ def register(request):
 def user_login(request):
 # If the request is a HTTP POST, try to pull out the relevant information.
 	if request.method == 'POST':
-		# Gather the username and password provided by the user.
-		# This information is obtained from the login form.
-		# We use request.POST.get('<variable>') as opposed
-		# to request.POST['<variable>'], because the
-		# request.POST.get('<variable>') returns None if the
-		# value does not exist, while request.POST['<variable>']
-		# will raise a KeyError exception.
 		username = request.POST.get('username')
 		password = request.POST.get('password')
 		# Use Django's machinery to attempt to see if the username/password
 		# combination is valid - a User object is returned if it is.
 		user = authenticate(username=username, password=password)
-		# If we have a User object, the details are correct.
-		# If None (Python's way of representing the absence of a value), no user
-		# with matching credentials was found.
 		if user:
 			# Is the account active? It could have been disabled.
 			if user.is_active:
-				# If the account is valid and active, we can log the user in.
-				# We'll send the user back to the homepage.
 				login(request, user)
 				return redirect(reverse('meme_portal:index'))
 			else:
@@ -157,8 +147,9 @@ def user_login(request):
 				return HttpResponse("Your Meme_portal account is disabled.")
 		else:
 			# Bad login details were provided. So we can't log the user in.
-			print(f"Invalid login details: {username}, {password}")
-			return HttpResponse("Invalid login details supplied.")
+			
+			messages.error(request,'username or password not correct')
+			return HttpResponseRedirect(reverse('meme_portal:login'))
 			# The request is not a HTTP POST, so display the login form.
 
 		# This scenario would most likely be a HTTP GET.
